6d8b5d1ed4439b029b2fe6eef571285e0246d6a6
viggo23111 <vbnymand@gmail.com>
2024-05-23 15:11:17 +0200

    categorygrid changes

--- components/CategoryGrid.js
+++ components/CategoryGrid.js
@@ -1,31 +1,35 @@
-import {useRouter} from "next/router";
+import { useRouter } from 'next/router';
+import { Card, Row, Col } from 'antd';
+import styles from '../styles/CategoryGrid.module.css'; // Custom CSS module
 
-
-function CategoryGrid ({categories}) {
-
+function CategoryGrid({ categories }) {
     const router = useRouter();
 
-
-    const clickCategory = (e) =>{
-
+    const clickCategory = (e) => {
         const url = {
-            pathname: "shop",
-            query: { ...router.query, category: e.target.id }
-        }
+            pathname: 'shop',
+            query: { ...router.query, category: e.currentTarget.id },
+        };
 
-
-        router.push(url)
-    }
+        router.push(url);
+    };
 
     return (
-        <div className="grid-container">
-            {categories.map(category => (
-                <div style={{zIndex:5}} id={category.id} key={category.id} onClick={clickCategory} className="grid-item">
-                    <h4  id={category.id}>{category.name}</h4>
-                </div>
+        <Row gutter={[16, 16]} className={styles.gridContainer}>
+            {categories.map((category) => (
+                <Col xs={24} sm={12} md={8} lg={6} key={category.id}>
+                    <Card
+                        id={category.id}
+                        onClick={clickCategory}
+                        hoverable
+                        className={styles.card}
+                    >
+                        <h4 id={category.id} className={styles.cardTitle}>{category.name}</h4>
+                    </Card>
+                </Col>
             ))}
-        </div>
+        </Row>
     );
-};
+}
 
 export default CategoryGrid;
